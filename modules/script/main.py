01deeffe4f6cebf8febb46e1255cf6a81d685a48
Elena <donna_vld@mail.ru>
2025-02-21 13:38:50 -0500

    Second step

--- modules/script/main.py
+++ modules/script/main.py
@@ -6,9 +6,12 @@ def main():
                     prog='hexlet-code',
                     description='Compares two configuration files and shows a difference.',
                     epilog='Enter 2 foles')
-    parser.add_argument("first_file", help="Path to the first configuration file")
-    parser.add_argument("second_file", help="Path to the second configuration file")
+    parser.add_argument("first_file", help="")
+    #Compares two configuration files and shows a difference.
+    parser.add_argument("second_file", help="")
+    #Path to the second configuration file
     args = parser.parse_args()
+    parser.add_argument('-f FORMAT','--format FORMAT') 
     
     # Здесь будет логика вычисления отличий между файлами
     print("Difference Calculator is not implemented yet.")
